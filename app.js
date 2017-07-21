b921c004fc4af759f108f188011c1f8c3dee7ee7
selvaantonyvijay <vijay@interactivelife.com>
2017-07-21 20:10:43 +0530

    Build destination path change

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.set('view engine', 'html');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 var routes = require('./config/routes');
